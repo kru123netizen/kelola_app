e1d1082a199de71122af886067ceaf5430fc03c5
kru123netizen <kurniarahman987@gmail.com>
2024-10-31 00:48:40 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/kelola_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kelola_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen/home_screen.dart';
+import 'screens/login_screen/login_screen.dart';
+import 'screens/otp_screen/otp_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'xxxxxxxxxxxx-g-famDgC3jx6VV4h-xxxxxx',
+              appId: '1:xxxxxxxxxxxx:android:xxxxxxxb7ea052854b0005',
+              messagingSenderId: 'xxxxxxxxxxxx',
+              projectId: 'flutterxxxxxxxxx-9xxxa'))
+      : await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter OTP Authentication',
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 0, 191, 166),
+      ),
+      home: LoginScreen(),
+      routes: <String, WidgetBuilder>{
+        '/otpScreen': (BuildContext ctx) => OtpScreen(),
+        '/homeScreen': (BuildContext ctx) => HomeScreen(),
+      },
+    );
+  }
+}
